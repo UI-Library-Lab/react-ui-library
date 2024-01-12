131881735a5558398d561d5596bdb006d47db440
behnamrhp <behnamrahimpour74@gmail.com>
2024-01-12 11:19:25 +0300

    [FIX]: storybook build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { resolve } from 'node:path'
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as packageJson from './package.json';
 import tsConfigPaths from 'vite-tsconfig-paths'
 import dts from 'vite-plugin-dts'
 import EsLint from 'vite-plugin-linter'
 import macrosPlugin from "vite-plugin-babel-macros"
+import { glob } from "glob";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
 
 const { EsLinter, linterPlugin } = EsLint
 
@@ -29,6 +30,7 @@ export default defineConfig((configEnv) => ({
     dts({
       insertTypesEntry: true,
       include: ['src/'],
+      exclude: ["**/*.stories.tsx"] 
     }),
     linterPlugin({
       include: ['./src/**/*.{ts,tsx}'],
@@ -42,11 +44,25 @@ export default defineConfig((configEnv) => ({
     lib: {
       entry: resolve('src', 'index.ts'),
       name: 'Ui LibrarygStorybook',
-      formats: ['es', 'umd'],
+      formats: ['es'],
       fileName: (format) => `uiLibrary-storybook.${format}.js`,
     },
     rollupOptions: {
-      external: [...Object.keys(packageJson.dependencies)]
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob.sync("src/**/*.{ts,tsx}").map((file) => [
+          // The name of the entry point
+          // lib/nested/foo.ts becomes nested/foo
+          relative("src", file.slice(0, file.length - extname(file).length)),
+          // The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     }
   }
 }));
